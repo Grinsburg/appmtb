69d084a280dd042ed1047d75de27dd93cc61e114
Grinsburg <smlynchik@mail.ru>
2020-01-10 00:59:31 +0300

    staging

--- scripts.js
+++ scripts.js
@@ -2,9 +2,7 @@
 
 const url = 'https://www.mtbank.by/currxml.php?ver=2';
 let ex = document.getElementById('ex');
-let exDiv = document.createElement('div')
-let div = document.createElement('div');
-let p = document.createElement('p');
+
 fetch(url)
   .then(res => res.text())
   .then(function(data) {
@@ -16,11 +14,15 @@ fetch(url)
         const city = rates[i].getAttribute('city');
         const address = rates[i].getAttribute('address');
         const label = rates[i].getAttribute('label');
+        let exDiv = document.createElement('div')
+        let div = document.createElement('div');
+        let p = document.createElement('p');
         let a = ex.appendChild(div);
         let b = a.appendChild(p)
         a.classList = 'depBlock';
         b.classList = 'department';
         let exd = a.appendChild(exDiv);
+        
         const department = rates[i];
         let rts = xmlDoc.getElementsByTagName('department')[i].innerHTML;
         exd.innerHTML = rts;
